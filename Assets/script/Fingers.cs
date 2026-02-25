c65f3cca59f08cbfc1e247123f10a7ea9600499e
Lance410247 <72780240+Lance410247@users.noreply.github.com>
2026-02-25 15:23:32 +0800

    加入手指
    加入手指鼠標
    加入按右鍵跟換手指數量
    BUG: 第三指圖片大小跑掉
    尚未實裝:不同手指有不同的手勢

---
+++ Assets/script/Fingers.cs
@@ -0,0 +1,70 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+public enum FingersNumber
+{
+    OneFinger,
+    TwoFinger,
+    ThirdFinger,
+    Pen
+}
+public class Fingers : MonoBehaviour
+{
+    public Sprite[] fingerPicture;
+    [SerializeField] private GameObject _gameObject;
+    private RectTransform rectTransform;
+    [SerializeField]
+    private FingersNumber currentFinger = FingersNumber.OneFinger;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        ChangeFingerImage(currentFinger);
+    }
+    // Update is called once per frame
+
+    void Update()
+    {
+        FollowMouse();
+
+        if (Input.GetMouseButtonDown(1)) // 右鍵
+        {
+            NextFinger();
+        }
+    }
+    public void ChangeFingerImage(FingersNumber number)
+    {
+        if (_gameObject.TryGetComponent<Image>(out var image))
+        {
+            image.sprite = fingerPicture[(int)FingersNumber.OneFinger];
+        }
+        try
+        {
+            image.sprite = fingerPicture[(int)number];
+
+        }
+        catch
+        {
+            Debug.Log("無法變換手指圖片");
+        }
+    }
+    public void NextFinger()
+    {
+        int next = ((int)currentFinger + 1) % System.Enum.GetValues(typeof(FingersNumber)).Length;
+        currentFinger = (FingersNumber)next;
+
+        ChangeFingerImage(currentFinger);
+    }
+    public void FollowMouse()
+    {
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            rectTransform.parent as RectTransform,
+            Input.mousePosition,
+            null,
+            out Vector2 localPos
+        );
+
+        rectTransform.anchoredPosition = localPos;
+    }
+   
+}
