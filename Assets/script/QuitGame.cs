82e73d2be767777b5c9b82a92e17eb11bf6b1478
Lance410247 <72780240+Lance410247@users.noreply.github.com>
2026-03-12 03:06:31 +0800

    主畫面美化
    美化主畫面

---
+++ Assets/script/QuitGame.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+
+
+    public void QuitThisGame() {
+
+
+        Application.Quit();
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
